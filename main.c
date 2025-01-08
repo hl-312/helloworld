81e697ea05dd1117657175a0e05d16a1e0d159c3
pwq <1066784440@qq.com>
2025-01-08 20:39:48 +0800

    led 闪烁——>简单实现占空比控制 led 亮度

--- main.c
+++ main.c
@@ -1,45 +1,38 @@
 #include <STC89C5xRC.H>
 
-typedef enum { RIGHT, LEFT } Direction;
-
-void delay_s( char num );			//delay for num seconds.
-void FlowLedP2( Direction dirt, char num, unsigned char drt );		// led lighting to flow to Direction in P2, fluence at num, duration of drt.
-
+void delay_ms( char num );			//delay for num ms.
 
 void main()
 {
-    P2 = 0xff;
-	FlowLedP2( LEFT, 1, 16);
-    while (1);
+    unsigned char i = 0;
+    while(1) {
+        if ( i % 8 == 0 ) {
+            P2 = 0xff;
+            P2 = P2^0x01;    //与 1 异或就是给该位取反
+        }
+        if ( i % 8 == 1 ) P2 = P2^0x02;
+        if ( i % 8 == 2 ) P2 = P2^0x04;
+        if ( i % 8 == 3 ) P2 = P2^0x08;
+        if ( i % 8 == 4 ) P2 = P2^0x10;
+        if ( i % 8 == 5 ) P2 = P2^0x20;
+        if ( i % 8 == 6 ) P2 = P2^0x40;
+        if ( i % 8 == 7 ) P2 = P2^0x80;
+        if ( i == 240) i = 0;
+        delay_ms(1);
+        i++;
+	};
 }
 
-//这个数据是实验模糊测的，不是计算的
-void delay_s( char num )
+
+
+void delay_ms( char num )
 {
 	if( num > 0 && num < 120 ){
 		for ( ; num > 0; num-- ){
 			unsigned int i;
-			for ( i = 0; i < 32800; i++ ){};
+			for ( i = 0; i < 33; i++ ){};
 		}
 		return ;
 	}
-	return ;
-}
-
-void FlowLedP2( Direction dirt, char num, unsigned char drt )
-{
-	unsigned char set = 0x01;
-	for ( ; drt > 0 ; drt-- ){
-		P2 = 0xff^set;
-		if ( dirt == RIGHT ) {
-			set <<= 1;
-			if ( set == 0 ) set = 0x01;
-		} else if (dirt == LEFT) {
-			set >>= 1;
-			if ( set == 0 ) set = 0x80;
-		}
-		delay_s( num );
-	}
-	P2 = 0xff;
 	return ;
 }
