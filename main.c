52ce36310d9f97edcfbcc798a0f6483db353742a
pwq <1066784440@qq.com>
2025-01-08 20:38:07 +0800

    完善流水灯，方向、流速（频率）、持续时间可控

--- main.c
+++ main.c
@@ -1,18 +1,45 @@
 #include <STC89C5xRC.H>
 
+typedef enum { RIGHT, LEFT } Direction;
+
+void delay_s( char num );			//delay for num seconds.
+void FlowLedP2( Direction dirt, char num, unsigned char drt );		// led lighting to flow to Direction in P2, fluence at num, duration of drt.
+
+
 void main()
 {
-	unsigned int i = 0;
-	unsigned char j = 0x01;
-	P2 = 0xff;
-	while(1)
-	{
-		i++;
-		if ( i == 33000 ){
-			P2 = 0xff^j;
-			j <<= 1;
-			if( j == 0) j = 0x01;
-			i = 0;
+    P2 = 0xff;
+	FlowLedP2( LEFT, 1, 16);
+    while (1);
+}
+
+//这个数据是实验模糊测的，不是计算的
+void delay_s( char num )
+{
+	if( num > 0 && num < 120 ){
+		for ( ; num > 0; num-- ){
+			unsigned int i;
+			for ( i = 0; i < 32800; i++ ){};
 		}
-	};
+		return ;
+	}
+	return ;
+}
+
+void FlowLedP2( Direction dirt, char num, unsigned char drt )
+{
+	unsigned char set = 0x01;
+	for ( ; drt > 0 ; drt-- ){
+		P2 = 0xff^set;
+		if ( dirt == RIGHT ) {
+			set <<= 1;
+			if ( set == 0 ) set = 0x01;
+		} else if (dirt == LEFT) {
+			set >>= 1;
+			if ( set == 0 ) set = 0x80;
+		}
+		delay_s( num );
+	}
+	P2 = 0xff;
+	return ;
 }
